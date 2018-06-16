25551eac4104323626904078df230a62ec5d4803
Yu Ishihara <ishihara.yu@gmail.com>
2018-06-16 23:17:53 +0900

    Add files

---
+++ Assets/Boy/Player.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //地面の判定に使うレイヤー
+    public LayerMask groundLayer;
+
+    //3Dモデル
+    public GameObject model;
+
+    //前のフレームでの位置
+    Vector3 prev;
+
+    //キー操作をチェックする
+    void Update()
+    {
+        PlayerSetting setting = gameObject.GetComponent<PlayerSetting>();
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            Move(setting.GetMove(KeyCode.UpArrow));
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            Move(setting.GetMove(KeyCode.DownArrow));
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            Move(setting.GetMove(KeyCode.LeftArrow));
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            Move(setting.GetMove(KeyCode.RightArrow));
+        }
+
+    }
+
+    //可能なら移動する
+    void Move(Vector2 force)
+    {
+        if (HasNextStep(force))
+        {
+            transform.Translate(force.x, 0, force.y);
+        }
+
+        //移動できなくても回転はする
+        Rotate(force);
+    }
+
+    //モデルを回転させる
+    void Rotate(Vector2 force)
+    {
+        model.transform.rotation = Quaternion.LookRotation(new Vector3(force.x, 0, force.y));
+    }
+
+    //移動先に地面があるかどうかチェック
+    bool HasNextStep(Vector2 force)
+    {
+        RaycastHit hit;
+
+        // Rayを飛ばして移動先の地面の有無を調べる
+        Ray ray = new Ray(transform.position + new Vector3(force.x, 0, force.y), Vector3.down);
+        if (Physics.Raycast(ray, out hit, 10.0f, groundLayer))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/Boy/PlayerSetting.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSetting : MonoBehaviour {
+
+    //移動方向のルール決め
+    [System.Serializable]
+    public class MoveInfo
+    {
+        [SerializeField] public KeyCode key;
+        [SerializeField] public Vector2 force;
+    }
+    public MoveInfo[] moveInfo;
+
+    //キー入力の方向に対しての移動Vectorを取得する
+    public Vector2 GetMove(KeyCode key)
+    {
+        for (int i = 0; i < moveInfo.Length; i++)
+        {
+            //Settingに登録があればVector2を返す
+            if (moveInfo[i].key == key) return moveInfo[i].force;
+        }
+        return Vector2.zero;
+    }
+}
